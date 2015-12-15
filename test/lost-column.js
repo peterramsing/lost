32fc41c973d3e9479f6beb92062b42699809a2c9
Zach Green <zach@alleyinteractive.com>
2015-12-15 09:30:08 -0500

    add some tests for optional lost-column order

--- test/lost-column.js
+++ test/lost-column.js
@@ -57,4 +57,181 @@ describe('lost-column', function() {
       'a:nth-child(3n) { margin-right: 0; }'
     );
   });
+
+  it('supports flexbox 1st', function() {
+    check(
+      'a { lost-column: flex 2/6 3 60px; }',
+      'a { flex: 0 0 auto; width: calc(99.99% * 2/6 - (60px - 60px * 2/6));' +
+      ' }\n' +
+      'a:nth-child(n) { margin-right: 60px; }\n' +
+      'a:last-child { margin-right: 0; }\n' +
+      'a:nth-child(3n) { margin-right: 0; }'
+    );
+  });
+
+  it('supports flexbox 2nd', function() {
+    check(
+      'a { lost-column: 2/6 flex 3 60px; }',
+      'a { flex: 0 0 auto; width: calc(99.99% * 2/6 - (60px - 60px * 2/6));' +
+      ' }\n' +
+      'a:nth-child(n) { margin-right: 60px; }\n' +
+      'a:last-child { margin-right: 0; }\n' +
+      'a:nth-child(3n) { margin-right: 0; }'
+    );
+  });
+
+  it('supports flexbox 3rd', function() {
+    check(
+      'a { lost-column: 2/6 3 flex 60px; }',
+      'a { flex: 0 0 auto; width: calc(99.99% * 2/6 - (60px - 60px * 2/6));' +
+      ' }\n' +
+      'a:nth-child(n) { margin-right: 60px; }\n' +
+      'a:last-child { margin-right: 0; }\n' +
+      'a:nth-child(3n) { margin-right: 0; }'
+    );
+  });
+
+  it('supports flexbox 4th', function() {
+    check(
+      'a { lost-column: 2/6 3 60px flex; }',
+      'a { flex: 0 0 auto; width: calc(99.99% * 2/6 - (60px - 60px * 2/6));' +
+      ' }\n' +
+      'a:nth-child(n) { margin-right: 60px; }\n' +
+      'a:last-child { margin-right: 0; }\n' +
+      'a:nth-child(3n) { margin-right: 0; }'
+    );
+  });
+
+  it('supports flexbox 5th', function() {
+    check(
+      'a { lost-column: 2/6 3 60px nth-of-type flex; }',
+      'a { flex: 0 0 auto; width: calc(99.99% * 2/6 - (60px - 60px * 2/6));' +
+      ' }\n' +
+      'a:nth-of-type(n) { margin-right: 60px; }\n' +
+      'a:last-of-type { margin-right: 0; }\n' +
+      'a:nth-of-type(3n) { margin-right: 0; }'
+    );
+  });
+
+  it('supports cycle 1st', function() {
+    check(
+      'a { lost-column: 2 2/3 40px flex; }',
+      'a { flex: 0 0 auto; width: calc(99.99% * 2/3 - (40px - 40px * 2/3));' +
+      ' }\n' +
+      'a:nth-child(n) { margin-right: 40px; }\n' +
+      'a:last-child { margin-right: 0; }\n' +
+      'a:nth-child(2n) { margin-right: 0; }'
+    );
+  });
+
+  it('supports cycle 2nd', function() {
+    check(
+      'a { lost-column: 2/3 2 40px flex; }',
+      'a { flex: 0 0 auto; width: calc(99.99% * 2/3 - (40px - 40px * 2/3));' +
+      ' }\n' +
+      'a:nth-child(n) { margin-right: 40px; }\n' +
+      'a:last-child { margin-right: 0; }\n' +
+      'a:nth-child(2n) { margin-right: 0; }'
+    );
+  });
+
+  it('supports cycle 3rd', function() {
+    check(
+      'a { lost-column: 2/3 40px 2 flex; }',
+      'a { flex: 0 0 auto; width: calc(99.99% * 2/3 - (40px - 40px * 2/3));' +
+      ' }\n' +
+      'a:nth-child(n) { margin-right: 40px; }\n' +
+      'a:last-child { margin-right: 0; }\n' +
+      'a:nth-child(2n) { margin-right: 0; }'
+    );
+  });
+
+  it('supports cycle 4th', function() {
+    check(
+      'a { lost-column: 2/3 40px flex 2; }',
+      'a { flex: 0 0 auto; width: calc(99.99% * 2/3 - (40px - 40px * 2/3));' +
+      ' }\n' +
+      'a:nth-child(n) { margin-right: 40px; }\n' +
+      'a:last-child { margin-right: 0; }\n' +
+      'a:nth-child(2n) { margin-right: 0; }'
+    );
+  });
+
+  it('supports cycle 5th', function() {
+    check(
+      'a { lost-column: 2/3 40px flex nth-of-type 2; }',
+      'a { flex: 0 0 auto; width: calc(99.99% * 2/3 - (40px - 40px * 2/3));' +
+      ' }\n' +
+      'a:nth-of-type(n) { margin-right: 40px; }\n' +
+      'a:last-of-type { margin-right: 0; }\n' +
+      'a:nth-of-type(2n) { margin-right: 0; }'
+    );
+  });
+
+  it('supports nth-of-type', function() {
+    check(
+      'a { lost-column: 1/3 nth-of-type; }',
+      'a { width: calc(99.99% * 1/3 - (30px - 30px * 1/3)); }\n' +
+      'a:nth-of-type(n) { float: left; margin-right: 30px; clear: none; }\n' +
+      'a:last-of-type { margin-right: 0; }\n' +
+      'a:nth-of-type(3n) { margin-right: 0; }\n' +
+      'a:nth-of-type(3n + 1) { clear: left; }'
+    );
+  });
+
+  it('supports nth-of-type 1st', function() {
+    check(
+      'a { lost-column: nth-of-type 2/3 40px flex 2; }',
+      'a { flex: 0 0 auto; width: calc(99.99% * 2/3 - (40px - 40px * 2/3));' +
+      ' }\n' +
+      'a:nth-of-type(n) { margin-right: 40px; }\n' +
+      'a:last-of-type { margin-right: 0; }\n' +
+      'a:nth-of-type(2n) { margin-right: 0; }'
+    );
+  });
+
+  it('supports nth-of-type 2nd', function() {
+    check(
+      'a { lost-column: 2/3 nth-of-type 40px flex 2; }',
+      'a { flex: 0 0 auto; width: calc(99.99% * 2/3 - (40px - 40px * 2/3));' +
+      ' }\n' +
+      'a:nth-of-type(n) { margin-right: 40px; }\n' +
+      'a:last-of-type { margin-right: 0; }\n' +
+      'a:nth-of-type(2n) { margin-right: 0; }'
+    );
+  });
+
+  it('supports nth-of-type 3rd', function() {
+    check(
+      'a { lost-column: 2/3 40px nth-of-type flex 2; }',
+      'a { flex: 0 0 auto; width: calc(99.99% * 2/3 - (40px - 40px * 2/3));' +
+      ' }\n' +
+      'a:nth-of-type(n) { margin-right: 40px; }\n' +
+      'a:last-of-type { margin-right: 0; }\n' +
+      'a:nth-of-type(2n) { margin-right: 0; }'
+    );
+  });
+
+  it('supports nth-of-type 4th', function() {
+    check(
+      'a { lost-column: 2/3 40px flex nth-of-type 2; }',
+      'a { flex: 0 0 auto; width: calc(99.99% * 2/3 - (40px - 40px * 2/3));' +
+      ' }\n' +
+      'a:nth-of-type(n) { margin-right: 40px; }\n' +
+      'a:last-of-type { margin-right: 0; }\n' +
+      'a:nth-of-type(2n) { margin-right: 0; }'
+    );
+  });
+
+  it('supports nth-of-type 5th', function() {
+    check(
+      'a { lost-column: 2/3 40px flex 2 nth-of-type; }',
+      'a { flex: 0 0 auto; width: calc(99.99% * 2/3 - (40px - 40px * 2/3));' +
+      ' }\n' +
+      'a:nth-of-type(n) { margin-right: 40px; }\n' +
+      'a:last-of-type { margin-right: 0; }\n' +
+      'a:nth-of-type(2n) { margin-right: 0; }'
+    );
+  });
+
 });
