25f1d8a3cd148317d691d8a67320e4fa7d583b8f
Alejandro Iglesias <ale.garciaiglesias@gmail.com>
2016-08-18 00:15:45 -0300

    Fix empty columns not taking intended width
    This happens on non-flexbox grid only. Since columns are floated and first column is empty or has a fixed positioned child, the column does not take intended width and righthand column takes over the place of the first column.
    
    ## Steps to reproduce
    
    ### Empty column
    
    ```html
    <div class="col-2"></div>
    <div class="col-10">my wide column</div>
    ```
    
    ```stylus
    .col-2
      lost-column: 2/12 12
    .col-10
      lost-column: 10/12 12
    ```
    
    ### Column with fixed positioned child
    
    ```html
    <div class="col-2">
      <div class="pos-f">my fixed content</div>
    </div>
    <div class="col-10">my wide column</div>
    ```
    
    ```stylus
    .col-2
      lost-column: 2/12 12
    .col-10
      lost-column: 10/12 12
    .pos-f
      position: fixed
    ```
    
    ## Solution
    
    Add a `min-height: 1px` rule to non-flexbox columns (flexbox columns don't suffer this issue).

--- test/lost-global-settings.js
+++ test/lost-global-settings.js
@@ -7,7 +7,7 @@ describe('lost-global-settings', function() {
     check(
       '@lost clearing left; \n' +
       'a { lost-column: 1/3; }',
-      'a { width: calc(99.9% * 1/3 - (30px - 30px * 1/3)); }\n' +
+      'a { min-height: 1px; width: calc(99.9% * 1/3 - (30px - 30px * 1/3)); }\n' +
       'a:nth-child(1n) { float: left; margin-right: 30px; clear: none; }\n' +
       'a:last-child { margin-right: 0; }\n' +
       'a:nth-child(3n) { margin-right: 0; float: right; }\n' +
@@ -16,7 +16,7 @@ describe('lost-global-settings', function() {
 
     check(
       'a { lost-column: 1/3; }',
-      'a { width: calc(99.9% * 1/3 - (30px - 30px * 1/3)); }\n' +
+      'a { min-height: 1px; width: calc(99.9% * 1/3 - (30px - 30px * 1/3)); }\n' +
       'a:nth-child(1n) { float: left; margin-right: 30px; clear: none; }\n' +
       'a:last-child { margin-right: 0; }\n' +
       'a:nth-child(3n) { margin-right: 0; float: right; }\n' +
