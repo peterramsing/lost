9cccc5efac1c66bc6784e6e8d964f393863ba223
Peter Ramsing <peter.ramsing@mac.com>
2016-08-23 22:29:59 -0700

    Updates lost core for ESLint

--- lost.js
+++ lost.js
@@ -2,20 +2,33 @@
 var postcss = require('postcss');
 var assign = require('object-assign');
 
+var lostAtRule = require('./lib/lost-at-rule');
+var lostUtility = require('./lib/lost-utility');
+var lostFlexContainer = require('./lib/lost-flex-container');
+var lostCenter = require('./lib/lost-center');
+var lostAlign = require('./lib/lost-align');
+var lostColumn = require('./lib/lost-column');
+var lostRow = require('./lib/lost-row');
+var lostWaffle = require('./lib/lost-waffle');
+var lostOffset = require('./lib/lost-offset');
+var lostMove = require('./lib/lost-move');
+var lostMasonryWrap = require('./lib/lost-masonry-wrap');
+var lostMasonryColumn = require('./lib/lost-masonry-column');
+
 // Lost At Rules and Declarations
 var libs = [
-  require('./lib/lost-at-rule'),
-  require('./lib/lost-utility'),
-  require('./lib/lost-flex-container'),
-  require('./lib/lost-center'),
-  require('./lib/lost-align'),
-  require('./lib/lost-column'),
-  require('./lib/lost-row'),
-  require('./lib/lost-waffle'),
-  require('./lib/lost-offset'),
-  require('./lib/lost-move'),
-  require('./lib/lost-masonry-wrap'),
-  require('./lib/lost-masonry-column')
+  lostAtRule,
+  lostUtility,
+  lostFlexContainer,
+  lostCenter,
+  lostAlign,
+  lostColumn,
+  lostRow,
+  lostWaffle,
+  lostOffset,
+  lostMove,
+  lostMasonryWrap,
+  lostMasonryColumn
 ];
 
 var defaultSettings = {
@@ -26,11 +39,11 @@ var defaultSettings = {
 };
 
 module.exports = postcss.plugin('lost', function lost(settings) {
-  settings = assign({}, defaultSettings, settings || {});
+  var theseSettings = assign({}, defaultSettings, settings || {});
 
-  return function (css) {
-    libs.forEach(function(lib) {
-      lib(css, settings);
+  return function executeLostGrid(css) {
+    libs.forEach(function executeEachLostRule(lib) {
+      lib(css, theseSettings);
     });
   };
 });
