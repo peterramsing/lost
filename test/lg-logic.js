4f4f63943a4a328a1d85db9565cb2dcf8b522052
Peter Ramsing <peter.ramsing@mac.com>
2016-11-11 12:49:02 -0800

    Modifies the width logic to work based on strings

--- test/lg-logic.js
+++ test/lg-logic.js
@@ -4,19 +4,26 @@ const expect = require('chai').expect;
 const lgLogic = require('../lib/_lg-logic.js');
 
 
-describe('columnLogic works as it should', () => {
+describe('widthValue works as it should', () => {
   it('gutter and rounder ✅', () => {
-    const foo = lgLogic.columnLogic('1/3', 30, 100);
+    const testCase = lgLogic.widthValue('1/3', '30px', 100);
 
-    const expectedResult = `calc(100% * 1/3 - (30 - 30 * 1/3))`;
+    const expectedResult = `calc(100% * 1/3 - (30px - 30px * 1/3))`;
 
-    expect(foo).to.equal(expectedResult);
+    expect(testCase).to.equal(expectedResult);
   });
   it('no gutter ✅', () => {
-    const foo = lgLogic.columnLogic('1/3', 0, 99.9);
+    const testCase = lgLogic.widthValue('1/3', '0', 99.9);
 
     const expectedResult = `calc(99.9% * 1/3)`;
 
-    expect(foo).to.equal(expectedResult);
+    expect(testCase).to.equal(expectedResult);
+  });
+  it('no gutter ✅ when gutter is undefined', () => {
+    const testCase = lgLogic.widthValue('1/3', undefined, 99.9);
+
+    const expectedResult = `calc(99.9% * 1/3)`;
+
+    expect(testCase).to.equal(expectedResult);
   });
 });
