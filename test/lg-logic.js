025130b438392bdce2732986614acb7cdbea84c9
Peter Ramsing <peter.ramsing@mac.com>
2016-11-11 13:01:39 -0800

    Makes compatible with Node v4 and v5

--- test/lg-logic.js
+++ test/lg-logic.js
@@ -1,28 +1,28 @@
 'use strict'
 
-const expect = require('chai').expect;
-const lgLogic = require('../lib/_lg-logic.js');
+var expect = require('chai').expect;
+var lgLogic = require('../lib/_lg-logic.js');
 
 
-describe('widthValue works as it should', () => {
+describe('calcValue works as it should', () => {
   it('gutter and rounder ✅', () => {
-    const testCase = lgLogic.widthValue('1/3', '30px', 100);
+    var testCase = lgLogic.calcValue('1/3', '30px', 100);
 
-    const expectedResult = `calc(100% * 1/3 - (30px - 30px * 1/3))`;
+    var expectedResult = `calc(100% * 1/3 - (30px - 30px * 1/3))`;
 
     expect(testCase).to.equal(expectedResult);
   });
   it('no gutter ✅', () => {
-    const testCase = lgLogic.widthValue('1/3', '0', 99.9);
+    var testCase = lgLogic.calcValue('1/3', '0', 99.9);
 
-    const expectedResult = `calc(99.9% * 1/3)`;
+    var expectedResult = `calc(99.9% * 1/3)`;
 
     expect(testCase).to.equal(expectedResult);
   });
   it('no gutter ✅ when gutter is undefined', () => {
-    const testCase = lgLogic.widthValue('1/3', undefined, 99.9);
+    var testCase = lgLogic.calcValue('1/3', undefined, 99.9);
 
-    const expectedResult = `calc(99.9% * 1/3)`;
+    var expectedResult = `calc(99.9% * 1/3)`;
 
     expect(testCase).to.equal(expectedResult);
   });
