a36beada2f9b0a1c04dfe3c5ad364161252705c5
Peter Ramsing <peter.ramsing@mac.com>
2016-11-16 21:36:22 -0800

    Adds units to calcValue logic

--- test/lg-logic.js
+++ test/lg-logic.js
@@ -5,10 +5,10 @@ var lgLogic = require('../lib/_lg-logic.js');
 
 
 describe('calcValue works as it should', () => {
-  it('gutter and rounder ✅', () => {
-    var testCase = lgLogic.calcValue('1/3', '30px', 100);
+  it('gutter, rounder, and unit ✅', () => {
+    var testCase = lgLogic.calcValue('1/3', '30px', 100, 'vw');
 
-    var expectedResult = `calc(100% * 1/3 - (30px - 30px * 1/3))`;
+    var expectedResult = `calc(100vw * 1/3 - (30px - 30px * 1/3))`;
 
     expect(testCase).to.equal(expectedResult);
   });
