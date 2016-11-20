43a608cd8f8ebe6ef6bf8501c12b02ecedcb2278
Peter Ramsing <peter.ramsing@mac.com>
2016-11-19 19:31:51 -0800

    Adds additional test

--- test/lg-gutter.js
+++ test/lg-gutter.js
@@ -21,6 +21,14 @@ describe('lost-gutter', function() {
       `div { margin: 25px $lost-gutter $lost-gutter; }`,
       `div { margin: 25px 30px 30px; }`
     );
+    check(
+      `div { padding: $lost-gutter; lost-column: 1/3 3 40px; }`,
+      `div { padding: 30px; width: calc(99.9% * 1/3 - (40px - 40px * 1/3)); }\n`+
+      `div:nth-child(1n) { float: left; margin-right: 40px; clear: none; }\n`+
+      `div:last-child { margin-right: 0; }\n`+
+      `div:nth-child(3n) { margin-right: 0; float: right; }\n`+
+      `div:nth-child(3n + 1) { clear: both; }`
+    );
   });
   it('replaces $lost-gutter with global when global is not default', function() {
     check(
