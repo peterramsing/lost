9688c5416f3ab7a9664d3e8d1cd324e2b88f8d5c
Peter Ramsing <peter.ramsing@mac.com>
2016-11-19 16:50:00 -0800

    Adds more tests for lost-gutter

--- test/lg-gutter.js
+++ test/lg-gutter.js
@@ -10,14 +10,22 @@ var check = require('./check');
 describe('lost-gutter', function() {
   it('replaces $lost-gutter with global', function() {
     check(
-      'div { padding: $lost-gutter; }',
-      'div { padding: 30px; }'
+      `div { padding: $lost-gutter; }`,
+      `div { padding: 30px; }`
+    );
+    check(
+      `div { margin: 25px $lost-gutter; }`,
+      `div { margin: 25px 30px; }`
+    );
+    check(
+      `div { margin: 25px $lost-gutter $lost-gutter; }`,
+      `div { margin: 25px 30px 30px; }`
     );
   });
   it('replaces $lost-gutter with global when global is not default', function() {
     check(
       `@lost gutter 40px; div { padding: $lost-gutter; }`,
-      'div { padding: 40px; }'
+      `div { padding: 40px; }`
     );
   });
 });
