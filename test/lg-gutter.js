39d224d9c2a55d8e294b825d4bc4270754a22ef9
Peter Ramsing <peter.ramsing@mac.com>
2016-11-23 22:21:42 -0800

    Changes syntax from lost-local-gutter to lost-gutter-local

--- test/lg-gutter.js
+++ test/lg-gutter.js
@@ -36,10 +36,10 @@ describe('lost-gutter', () => {
 });
 
 
-describe('lost-local-gutter', () => {
-  it('replaces $lost-local-gutter', () => {
+describe('lost-gutter-local', () => {
+  it('replaces $lost-gutter-local', () => {
     check(
-      `div { padding: $lost-local-gutter; lost-column: 1/3; lost-column-gutter: 50px; }`,
+      `div { padding: $lost-gutter-local; lost-column: 1/3; lost-column-gutter: 50px; }`,
       `div { padding: 50px; width: calc(99.9% * 1/3 - (50px - 50px * 1/3)); }\n`+
       `div:nth-child(1n) { float: left; margin-right: 50px; clear: none; }\n`+
       `div:last-child { margin-right: 0; }\n`+
@@ -47,7 +47,7 @@ describe('lost-local-gutter', () => {
       `div:nth-child(3n + 1) { clear: both; }`
     );
     check(
-      `div { padding: $lost-local-gutter; lost-column: 1/3; lost-column-gutter: 0; }`,
+      `div { padding: $lost-gutter-local; lost-column: 1/3; lost-column-gutter: 0; }`,
       `div { padding: 0; width: calc(99.9% * 1/3); }\n`+
       `div:nth-child(1n) { float: left; margin-right: 0; clear: none; }\n`+
       `div:last-child { margin-right: 0; }\n`+
@@ -57,7 +57,7 @@ describe('lost-local-gutter', () => {
   });
   it('works on shortcut lost-column', () => {
     check(
-      `div { padding: $lost-local-gutter; lost-column: 1/3 3 70px;}`,
+      `div { padding: $lost-gutter-local; lost-column: 1/3 3 70px;}`,
       `div { padding: 70px; width: calc(99.9% * 1/3 - (70px - 70px * 1/3));}\n`+
       `div:nth-child(1n) { float: left; margin-right: 70px; clear: none;}\n`+
       `div:last-child { margin-right: 0;}\n`+
@@ -67,7 +67,7 @@ describe('lost-local-gutter', () => {
   });
   it('works on shortcut lost-waffle', () => {
     check(
-      `div { padding: $lost-local-gutter; lost-waffle: 1/3 3 20px;}`,
+      `div { padding: $lost-gutter-local; lost-waffle: 1/3 3 20px;}`,
       `div { padding: 20px; width: calc(99.9% * 1/3 - (20px - 20px * 1/3)); height: calc(99.9% * 1/3 - (20px - 20px * 1/3));}\n`+
       `div:nth-child(1n) { float: left; margin-right: 20px; margin-bottom: 20px; clear: none;}\n` +
       `div:last-child { margin-right: 0; margin-bottom: 0;}\n` +
@@ -78,13 +78,13 @@ describe('lost-local-gutter', () => {
   });
   it('works on shortcut lost-offset', () => {
     check(
-      `div { padding: $lost-local-gutter; lost-offset: 1/3 3 100px;}`,
+      `div { padding: $lost-gutter-local; lost-offset: 1/3 3 100px;}`,
       `div { padding: 100px; margin-left: calc(99.9% * (-1/3 * -1) - (100px - 100px * (-1/3 * -1)) + 100px) !important;}`
     );
   });
   it('works on shortcut lost-center', () => {
     check(
-      `div { padding: $lost-local-gutter; lost-center: 400px 60px;}`,
+      `div { padding: $lost-gutter-local; lost-center: 400px 60px;}`,
       `div { padding: 60px; max-width: 400px; margin-left: auto; margin-right: auto; padding-left: 60px; padding-right: 60px;}\n` +
       `div:before { content: ''; display: table;}\n` +
       `div:after { content: ''; display: table; clear: both;}`
@@ -92,26 +92,26 @@ describe('lost-local-gutter', () => {
   });
   it('works on shortcut lost-masonry-wrap', () => {
     check(
-      `div { padding: $lost-local-gutter; lost-masonry-wrap: flex 60px;}`,
+      `div { padding: $lost-gutter-local; lost-masonry-wrap: flex 60px;}`,
       `div { padding: 60px; display: flex; flex-flow: row wrap; margin-left: -30px; margin-right: -30px;}`
     );
   });
   it('works on shortcut lost-masonry-column', () => {
     check(
-      `div { padding: $lost-local-gutter; lost-masonry-column: 1/3 15px;}`,
+      `div { padding: $lost-gutter-local; lost-masonry-column: 1/3 15px;}`,
       `div { padding: 15px; float: left; width: calc(99.9% * 1/3 - 15px); margin-left: 7.5px; margin-right: 7.5px;}`
     );
   });
   it('works on shortcut lost-row', () => {
     check(
-      `div { padding: $lost-local-gutter; lost-row: 1/3 70px;}`,
+      `div { padding: $lost-gutter-local; lost-row: 1/3 70px;}`,
       `div { padding: 70px; width: 100%; height: calc(99.9% * 1/3 - (70px - 70px * 1/3)); margin-bottom: 70px;}\n` +
       `div:last-child { margin-bottom: 0;}`
     );
   });
   it('takes global if no local one is given', () => {
     check(
-      `div { padding: $lost-local-gutter; lost-column: 1/3;}`,
+      `div { padding: $lost-gutter-local; lost-column: 1/3;}`,
       `div { padding: 30px; width: calc(99.9% * 1/3 - (30px - 30px * 1/3));}\n` +
       `div:nth-child(1n) { float: left; margin-right: 30px; clear: none;}\n` +
       `div:last-child { margin-right: 0;}\n` +
@@ -121,7 +121,7 @@ describe('lost-local-gutter', () => {
   });
   it('allows for multiple uses of the variable', () => {
     check(
-      `div { padding: $lost-local-gutter; lost-column: 1/3 3 20px; margin-top: $lost-local-gutter;}`,
+      `div { padding: $lost-gutter-local; lost-column: 1/3 3 20px; margin-top: $lost-gutter-local;}`,
       `div { padding: 20px; width: calc(99.9% * 1/3 - (20px - 20px * 1/3)); margin-top: 20px;}\n` +
       `div:nth-child(1n) { float: left; margin-right: 20px; clear: none;}\n` +
       `div:last-child { margin-right: 0;}\n` +
