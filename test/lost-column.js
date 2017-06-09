00537a5d297f8c525dad6d9364c9ae1e15fd92dd
Fin Hopkins <fin.hopkins@boston.gov>
2017-06-09 13:59:05 -0400

    Limits flex-basis for IE10/11
    Fix for IE10/11 not taking box-sizing into account when using flex-basis. Adding a max-width of the same value keeps IE from growing the element too large.

--- test/lost-column.js
+++ test/lost-column.js
@@ -52,6 +52,7 @@ describe('lost-column', function() {
       'a { lost-column: 2/6 3 60px flex; }',
       'a { flex-grow: 0; flex-shrink: 0; ' +
       'flex-basis: calc(99.9% * 2/6 - (60px - 60px * 2/6)); ' +
+      'max-width: calc(99.9% * 2/6 - (60px - 60px * 2/6)); ' +
       'width: calc(99.9% * 2/6 - (60px - 60px * 2/6)); }\n' +
       'a:nth-child(1n) { margin-right: 60px; margin-left: 0; }\n' +
       'a:last-child { margin-right: 0; }\n' +
