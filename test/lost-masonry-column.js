4c4dc7202cbf0a99f53e4ea6d44b415b57315189
Peter Ramsing <peter.ramsing@mac.com>
2017-07-01 11:25:36 -0700

    Adds a test for long-form flexbox

--- test/lost-masonry-column.js
+++ test/lost-masonry-column.js
@@ -30,6 +30,14 @@ describe('lost-masonry-column', function() {
   });
 
   describe('flexbox', function() {
+    it('supports flexbox with long-form property', function() {
+      check(
+        'a { lost-masonry-column: 60px; lost-masonry-column-flexbox: flex; }',
+        'a { flex: 0 0 auto; width: calc(99.9% * 60px - 30px);' +
+        ' margin-left: 15px; margin-right: 15px; }'
+      )
+    });
+
     it('supports a custom gutter', function() {
       check(
         'a { lost-masonry-column: 60px flex; }',
