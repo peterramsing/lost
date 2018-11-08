4c1372864371f388dd33bfd67fb07ea03c997af1
Peter Ramsing <peter.ramsing@mac.com>
2018-11-07 19:30:26 -0800

    Migrates to spec files instead of relying the test directory

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,8 @@
 module.exports = {
     env: {
         es6: true,
-        node: true
+        node: true,
+        mocha: true
     },
     plugins: ['prettier'],
     extends: 'eslint:recommended',
