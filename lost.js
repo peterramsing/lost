ae9de55d2fcfd52fdd42ad671f042eee13476ad5
Peter Ramsing <peter.ramsing@mac.com>
2019-02-09 14:51:19 -0800

    :art: Refactors for clarity and stability

--- lost.js
+++ lost.js
@@ -1,24 +1,25 @@
 // Module dependencies
-var postcss = require('postcss');
-var assign = require('object-assign');
+const postcss = require('postcss');
+const assign = require('object-assign');
 
-var lostAtRule = require('./lib/lost-at-rule');
-var lostUtility = require('./lib/lost-utility');
-var lostFlexContainer = require('./lib/lost-flex-container');
-var lostCenter = require('./lib/lost-center');
-var lostAlign = require('./lib/lost-align');
-var lostColumn = require('./lib/lost-column');
-var lostRow = require('./lib/lost-row');
-var lostWaffle = require('./lib/lost-waffle');
-var lostOffset = require('./lib/lost-offset');
-var lostMove = require('./lib/lost-move');
-var lostMasonryWrap = require('./lib/lost-masonry-wrap');
-var lostMasonryColumn = require('./lib/lost-masonry-column');
-var lostGutter = require('./lib/lost-gutter');
-var lostVars = require('./lib/lost-vars');
+const lostAlign = require('./lib/lost-align');
+const lostAtRule = require('./lib/lost-at-rule');
+const lostCenter = require('./lib/lost-center');
+const lostColumn = require('./lib/lost-column');
+const lostFlexContainer = require('./lib/lost-flex-container');
+const lostGutter = require('./lib/lost-gutter');
+const lostMasonryColumn = require('./lib/lost-masonry-column');
+const lostMasonryWrap = require('./lib/lost-masonry-wrap');
+const lostMove = require('./lib/lost-move');
+const lostOffset = require('./lib/lost-offset');
+const lostRow = require('./lib/lost-row');
+const lostUtility = require('./lib/lost-utility');
+const lostVars = require('./lib/lost-vars');
+const lostWaffle = require('./lib/lost-waffle');
 
 // Lost At Rules and Declarations
-var libs = [
+// NOTE: Order Matters
+const libs = [
   lostAtRule,
   lostVars,
   lostGutter,
@@ -35,7 +36,7 @@ var libs = [
   lostMasonryColumn
 ];
 
-var defaultSettings = {
+const defaultSettings = {
   gutter: '30px',
   flexbox: 'no-flex',
   cycle: 'auto',
@@ -45,12 +46,11 @@ var defaultSettings = {
   direction: 'ltr'
 };
 
-module.exports = postcss.plugin('lost', function lost(settings) {
-  var theseSettings = assign({}, defaultSettings, settings || {});
-
-  return function executeLostGrid(css, result) {
-    libs.forEach(function executeEachLostRule(lib) {
-      lib(css, theseSettings, result);
+module.exports = postcss.plugin('lost', settings => {
+  let runSettings = assign({}, defaultSettings, settings | {});
+  return (css, result) => {
+    libs.forEach(lib => {
+      lib(css, runSettings, result);
     });
   };
 });
