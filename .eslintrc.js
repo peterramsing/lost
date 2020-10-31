0fcb127a219ee058d88649089769bb171eb8500e
Peter Ramsing <peter.ramsing@xpo.com>
2020-10-31 15:25:02 -0700

    👕Cleans up lint configs for new versions

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,25 @@
 module.exports = {
-    env: {
-        es6: true,
-        node: true
-    },
-    plugins: ['prettier'],
-    extends: 'eslint:recommended',
-    parserOptions: {
-        sourceType: 'module'
-    },
-    rules: {
-        'eol-last': 2,
-        'no-multiple-empty-lines': [2, { max: 2, maxEOF: 1 }],
-        indent: ['error', 2],
-        'linebreak-style': ['error', 'unix'],
-        quotes: [
-            'error',
-            'single',
-            {
-                avoidEscape: true
-            }
-        ],
-        semi: ['error', 'always'],
-        'prettier/prettier': 'error'
-    }
+  env: {
+    es6: true,
+    node: true,
+  },
+  plugins: ['prettier'],
+  extends: 'eslint:recommended',
+  parserOptions: {
+    sourceType: 'module',
+  },
+  rules: {
+    'eol-last': 2,
+    'no-multiple-empty-lines': [2, { max: 2, maxEOF: 1 }],
+    'linebreak-style': ['error', 'unix'],
+    quotes: [
+      'error',
+      'single',
+      {
+        avoidEscape: true,
+      },
+    ],
+    semi: ['error', 'always'],
+    'prettier/prettier': 'error',
+  },
 };
