9b563dcbf36ffe9fae9dfaa68977ffec9bd67fee
Peter Ramsing <peter.ramsing@mac.com>
2022-05-14 11:18:37 -0700

    Adds failing test for async plugins

--- test/check.js
+++ test/check.js
@@ -4,11 +4,25 @@ var expect = require('chai').expect;
 var lost = require('../lost');
 var cleanCss = require('clean-css');
 var postcss = require('postcss');
+const postcssPresetEnv = require('postcss-preset-env');
 
-module.exports = function check(input, output, opts) {
-  var processor = postcss([lost(opts)]);
+module.exports = function check(input, output, options) {
+  var processor = postcss([lost(options)]);
   var cleanInput = new cleanCss({}).minify(processor.process(input).css);
   var cleanOutput = new cleanCss({}).minify(output);
 
   expect(cleanInput.styles).to.equal(cleanOutput.styles);
+};
+
+module.exports = function pluginCheck(input, output, options) {
+  let cleanInput, cleanOutput;
+
+  postcss([postcssPresetEnv({ stage: 0 }), lost(options)])
+    .process(input)
+    .then(async (result) => {
+      cleanInput = new cleanCss({}).minify(processor.process(input).css);
+      cleanOutput = new cleanCss({}).minify(output);
+
+      expect(cleanInput.styles).to.equal(cleanOutput.styles);
+    });
 };
---
+++ test/lost-nesting.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var pluginCheck = require('./check');
+
+describe('nesting-queries', () => {
+  it('should retain media nesting', () => {
+    pluginCheck(
+      `
+      body {
+        background: green;
+        @media (min-width: 768px) {
+            background: red;
+        }
+      }
+      `,
+      `
+      body {
+        background: green;
+        @media (min-width: 768px) {
+            background: red;
+        }
+      }
+      `
+    );
+  });
+});
