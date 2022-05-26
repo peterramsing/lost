cb27a32450862d2e13eb4d1f36f49990dfa8ba91
Peter Ramsing <peter.ramsing@mac.com>
2022-05-26 13:36:47 -0700

    Corrects experimental CSS nesting test case

--- test/lost-nesting.js
+++ test/lost-nesting.js
@@ -1,13 +1,18 @@
 'use strict';
 
-var pluginCheck = require('./check');
+const check = require('./check');
+const postcss = require('postcss');
+const postcssPresetEnv = require('postcss-preset-env');
+const lost = require('../lost');
+const expect = require('chai').expect;
 
 describe('nesting-queries', () => {
-  it('should retain media nesting', () => {
-    pluginCheck(
+  it('should retain media nesting with just LostGrid processing', () => {
+    check(
       `
       body {
         background: green;
+        
         @media (min-width: 768px) {
             background: red;
         }
@@ -16,11 +21,47 @@ describe('nesting-queries', () => {
       `
       body {
         background: green;
+        
         @media (min-width: 768px) {
             background: red;
         }
       }
       `
     );
+  });
+  it('should allow plugins to function', () => {
+    const input = `
+    body {
+      background: green;
+      
+      @media (min-width: 768px) {
+          & {
+            background: red;
+          }
+      }
+    }
+    `;
+    const output = `
+    body {
+      background: green
+    }
+@media (min-width: 768px) {
+          body {
+            background: red;
+          }
+      }
+    `;
+    postcss([
+      lost(),
+      postcssPresetEnv({
+        features: {
+          'nesting-rules': true,
+        },
+      }),
+    ])
+      .process(input)
+      .then(async (result) => {
+        expect(output).to.equal(result.css);
+      });
   });
 });
